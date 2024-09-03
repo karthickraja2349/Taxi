723dbecab1cd5607274a14548cf15865a4a52cf5
karthickraja2349 <133229189+karthickraja2349@users.noreply.github.com>
2024-09-03 14:28:24 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,8 @@
+import Taxi.KuffMannTaxis;
+
+public class User {
+    public static void main(String[]args){
+        KuffMannTaxis kuffMannTaxis = new KuffMannTaxis();
+        kuffMannTaxis.start();
+    }
+}
